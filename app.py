82e75cd39f94eec539e9b4566aae05063f864642
Hassan <hassansmallah@gmail.com>
2021-12-17 17:07:05 +0300

    create post + environ

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import pymongo
 from pymongo import MongoClient
+import environ  # for .env
 
-connection = 'mongodb+srv://admin:<password>@cluster0.rthfj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
+username = env('username')
+password = env('password')
+
+connection = 'mongodb+srv://{}:{}@cluster0.rthfj.mongodb.net/test?retryWrites=true&w=majority'.format(username, password)
 cluster = MongoClient(connection)
 db = cluster["test"]
+collection = db['test']
 
+post = {
+    "_id": 1,
+    "name": "Jim",
+    "score": 8
+}
 
-print(cluster)
+collection.insert_one(post)
